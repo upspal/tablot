266d690e5fee7a898ccb5034f4866ebe2da325ae
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2020-06-15 15:59:36 +0530

    g-sheet api + gspread

--- bot.py
+++ bot.py
@@ -2,17 +2,25 @@ import discord
 import os
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 # table data
-tableData = [
-    ['number', 'name', 'role'],
-    ['1', 'rachit gupta', 'loser'],
-    ['2', 'mihir aggarwal', 'pro loser']
-]
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+gClient = gspread.authorize(creds)
+sheetName = input('Enter the name of your sheet: ')
+sheet = gClient.open(sheetName).sheet1
+data = sheet.get_all_records()
+
+tableData = [[key for key in data[0].keys()]]
+for i in data:
+    val = [value for value in i.values()]
+    tableData.append(val)
 
 table = AsciiTable(tableData)
 
@@ -22,14 +30,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
 
     if message.content.startswith('$hello'):
         await message.channel.send(f'```{table.table}```')
 
 client.run(TOKEN)
-
-# TODO - recognize gsheet link (public)
-# TODO - get cell data using gsheet api
-# TODO - use https://pypi.org/project/terminaltables/ and generate table
