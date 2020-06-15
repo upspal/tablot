43a128152ed4277e5fcf7d04a3078375ad2b1962
Rachit Gupta <guptarachit2004@gmail.com>
2020-06-15 17:15:42 +0530

    show filename + command decision

--- bot.py
+++ bot.py
@@ -13,16 +13,6 @@ client = discord.Client()
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 gClient = gspread.authorize(creds)
-sheetName = input('Enter the name of your sheet: ')
-sheet = gClient.open(sheetName).sheet1
-data = sheet.get_all_records()
-
-tableData = [[key for key in data[0].keys()]]
-for i in data:
-    val = [value for value in i.values()]
-    tableData.append(val)
-
-table = AsciiTable(tableData)
 
 @client.event
 async def on_ready():
@@ -31,7 +21,43 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.content.startswith('$hello'):
-        await message.channel.send(f'```{table.table}```')
+    if message.content.startswith(f'$ts show "'):
+        if message.content[-1] != '"':
+            await message.channel.send('bhai naam toh dhang se likh le')
+        else:
+            try:
+                a = message.content.split('"')
+                sheet = gClient.open(a[1]).sheet1
+                data = sheet.get_all_records()
+
+                tableData = [[key for key in data[0].keys()]]
+                for i in data:
+                    val = [value for value in i.values()]
+                    tableData.append(val)
+
+                table = AsciiTable(tableData)
+                await message.channel.send(f'```{table.table}```')
+            except:
+                await message.channel.send('pls junos dis is incorrect file name')
 
 client.run(TOKEN)
+
+# web scraping - future?
+# email: mihir-462@tablot-280404.iam.gserviceaccount.com
+
+'''
+Commands: 
+1. $ts about - bot introduces itself ($hello --> $about)
+2. $ts ping - check the bot's latency (j)
+3. $ts github - display github repo (j)
+4. $ts stats - as the name suggests 
+4. ts show "file name"
+
+6. $ts sheet name rachit
+serial number: 1
+name: Rachit
+class: XI - C
+stream: science
+'''
+
+
