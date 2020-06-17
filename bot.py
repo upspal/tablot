5dfd75b91bad993db278399a46a191bc3d7db038
upspal <uppal.pranav@gmail.com>
2020-06-17 23:25:01 +0530

    refactored prefix

--- bot.py
+++ bot.py
@@ -7,13 +7,15 @@ from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+#TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-
+prefix = '$ts'
 # table data
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 gClient = gspread.authorize(creds)
+
 
 @client.event
 async def on_ready():
@@ -21,15 +23,17 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$ts show '):
+    if message.content.startswith(f'{prefix} show '):
         if message.content[-1] != '"':
             a = message.content.split('"')
             if len(a) < 3:
-                await message.channel.send('The command syntax is incorrect. Please use `$ts help` to check the commands.')
+                await message.channel.send(
+                    'The command syntax is incorrect. Please use `$ts help` to check the commands.')
             try:
                 sheet = gClient.open(a[1]).sheet1
             except:
-                await message.channel.send('Make sure the sheet name is correct. Also, if you haven\'t already, please share your google sheet with `mihir-462@tablot-280404.iam.gserviceaccount.com`.')
+                await message.channel.send(
+                    'Make sure the sheet name is correct. Also, if you haven\'t already, please share your google sheet with `mihir-462@tablot-280404.iam.gserviceaccount.com`.')
             data = sheet.findall(a[2][1:])
             tableData = []
             tableData.append(sheet.row_values(1))
@@ -41,7 +45,7 @@ async def on_message(message):
                     break
                 else:
                     for j in range(len(i)):
-                        final += f'{tableData[0][j]}: {tableData[tableData.index(i)+1][j]}\n'
+                        final += f'{tableData[0][j]}: {tableData[tableData.index(i) + 1][j]}\n'
                     await message.channel.send(f'```{final}```')
                     final = ''
         else:
@@ -58,22 +62,25 @@ async def on_message(message):
                 table = AsciiTable(tableData)
                 await message.channel.send(f'```{table.table}```')
             except:
-                await message.channel.send('Make sure the sheet name is correct. Also, if you haven\'t already, please share your google sheet with `mihir-462@tablot-280404.iam.gserviceaccount.com`.')
+                await message.channel.send(
+                    'Make sure the sheet name is correct. Also, if you haven\'t already, please share your google sheet with `mihir-462@tablot-280404.iam.gserviceaccount.com`.')
 
-    if message.content.startswith('$ts about'):
-        embed = discord.Embed(title='Thanks for adding me to your server! :heart:', description='To get started, simply share your google sheet with me at `mihir-462@tablot-280404.iam.gserviceaccount.com`, and type `$ts help` for a list of commands', colour=1499502)\
-        .add_field(
+    if message.content.startswith(f'{prefix} about'):
+        embed = discord.Embed(title='Thanks for adding me to your server! :heart:',
+                              description='To get started, simply share your google sheet with me at `mihir-462@tablot-280404.iam.gserviceaccount.com`, and type `$ts help` for a list of commands',
+                              colour=1499502) \
+            .add_field(
             name='Tablot',
             value='Tablot helps you conveniently display your google sheets data on a discord server.',
             inline=False).add_field(
             name='Contribute',
             value='We gladly accept contributions. To get started, ' +
-            'check out [Tablot\'s GitHub repo](https://github.com/techsyndicate/tablot).',
+                  'check out [Tablot\'s GitHub repo](https://github.com/techsyndicate/tablot).',
             inline=False
         ).set_footer(text='Made by Tech Syndicate', icon_url='https://techsyndicate.co/img/logo.png')
         await message.channel.send(embed=embed)
 
-    if message.content.startswith(f'$ts stats'):
+    if message.content.startswith(f'{prefix} stats'):
         embed = discord.Embed(
             title=f'Description',
             description='',
@@ -86,25 +93,31 @@ async def on_message(message):
             name='Latency',
             value=f'{round(client.latency * 1000, 2)}ms',
             inline=True
-        ).add_field(name='User Count', value=len([s.members for s in client.guilds])).set_footer(text='Made by Tech Syndicate', icon_url='https://techsyndicate.co/img/logo.png')
+        ).add_field(name='User Count',
+            value=len([s.members for s in client.guilds]))\
+            .set_footer(text='Made by Tech Syndicate', icon_url='https://techsyndicate.co/img/logo.png')
         await message.channel.send(embed=embed)
-    
-    if message.content.startswith("$ts help"):
+
+    if message.content.startswith(f'{prefix} help'):
         embed = discord.Embed(
-        title="Tablot's commands:",
-        colour=1499502,
-        description="""
+            title="Tablot's commands:",
+            colour=1499502,
+            description="""
 > To use a  command type `$ts <command>`.
 
-**General**
+**General:**
+
 `about` - To know about the bot.
 `stats` - To check the bot's stats.
 
-**Google Sheets **
+**Google Sheets:**
+
 `show "file name"` - To display the whole table
 `show "file name" value` - To display rows of specific value
 """).set_footer(text='Made by Tech Syndicate', icon_url='https://techsyndicate.co/img/logo.png')
         await message.channel.send(embed=embed)
+
+
 client.run(TOKEN)
 
 # web scraping - future?
